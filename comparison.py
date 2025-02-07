7c10739cb12c4a130182cf20fb7188eedcf04cf8
amin <37590023+ad3409@users.noreply.github.com>
2025-02-07 20:18:47 +0100

    Update comparison.py

--- comparison.py
+++ comparison.py
@@ -1,16 +1,9 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2821
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+ import time
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score, f1_score
 
-\f0\fs24 \cf0 import time\
-import random\
-import numpy as np\
-import matplotlib.pyplot as plt\
-from sklearn.metrics import accuracy_score, f1_score\
-\
 # Simulating responses for three models\
 models = ["RAG ONLY", "RAG + PDF for Training", "Without RAG (Only PDF)"]\
 \
